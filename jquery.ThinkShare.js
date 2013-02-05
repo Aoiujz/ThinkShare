4eb9867a97290a06feb8dd51c5384b7b51adb451
麦当苗儿 <zuojiazi@vip.qq.com>
2013-02-05 16:42:47 +0800

    修改自动加载的逻辑判断

--- jquery.ThinkShare.js
+++ jquery.ThinkShare.js
@@ -9,11 +9,22 @@
  +-------------------------------------------------------------------
  */
 (function(){
+var
 	/* 当前脚本文件名 */
-	var __FILE__ = $("script").last().attr("src");
+	__FILE__ = $("script").last().attr("src"),
+
+	/* 分享插件对象 */
+	ThinkShare,
+
+	/* 分享插件默认配置 */
+	defaults = {
+		"share"    : [], //需要分享到的平台，空表示分享到所有支持的平台 可以在标签上使用data-share更改
+		"style"    : "text", //分享工具条风格，可在标签上使用data-style属性更改
+		"header"   : "分享到：" //分享工具前缀
+	},
 
 	/* 分享平台配置 */
-	var config = {
+	config = {
 		//QQ空间分享配置
 		"qzone" : {
 			"api"   : "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey",
@@ -34,16 +45,10 @@
 			"appkey"    : "",
 			"ralateUid" : ""
 		}
-	}
-
-	/* 分享插件默认配置 */
-	var defaults = {
-		"share"    : [], //需要分享到的平台，空表示分享到所有支持的平台 可以在标签上使用data-share更改
-		"style"    : "text" //分享工具条风格，可在标签上使用data-style属性更改
-	}
+	},
 
 	/* 创建分享参数 */
-	var params = {
+	params = {
 		//组装QQ空间分享参数
 		"qzone" : function(value){
 			return [
@@ -79,90 +84,107 @@
 			].join("&");
 		}
 
-	}
+	};
 
-	var ThinkShare = function(element, options){
-		//初始化配置参数
-		var self = this, ele = $(element), share_value;
-		var options = $.extend({}, defaults, options || {}, ele.data());
-
-		//获取分享数据
-		share_value = _get_share_data();
-
-		//解析字符串分享参数
-		if($.type(options.share) === "string") options.share = options.share.split(',');
-
-		//设置工具栏样式
-		ele.css("display", "inline-block").addClass("think-share-" + options.style);
-
-		//创建分享按钮
-		ele.append(_getShareBtnHTML());
-		
-		//获取分享数据
-		function _get_share_data(){
-			var data     = {};
-			data.url     = encodeURIComponent(options.url || window.location.href);
-			data.title   = encodeURIComponent(options.title || $("title").text());
-			data.pic     = encodeURIComponent(options.pic || "");
-			data.summary = encodeURIComponent(options.summary || "");
-			_delete("url", "title", "pic", "summary");
-			return data;
-		}
-
-		//获取指定分享按钮html代码
-		function _getShareBtnHTML(){
-			var html = ["<span>分享到：</span>"];
-			if(options.share.length == 0){
-				$.each(config, function(name){
-					html.push(_createShareBtn(name));
-				});
-			} else {
-				for(i in options.share){
-					html.push(_createShareBtn(options.share[i]));
-				}
-			}
-			return html.join("");
-		}
-
-		//创建分享按钮
-		function _createShareBtn(name){
-			return [
-				"<a class=\"share-icon-",
-				name,
-				"\" target=\"_blank\" href=\"",
-				_get_share_url(name),
-				"\">",
-				config[name].title,
-				"</a>"
-			].join("");
-		}
-
-		//获取分享url
-		function _get_share_url(name){
-			var param = params[name](share_value);
-			return [config[name].api, "?", param].join("");
-		}
-
-		//删除选项
-		function _delete(){
-			for(i in arguments){
-				delete options[arguments[i]];
-			}  
-		}
-	}
-
-	/* 定义JQUERY分享插件 */
-	$.fn.ThinkShare = function(options){
-		$(this).each(function(){
-			new ThinkShare(this, options);
-		});
-	}
+/**
+ * 构造方法，用于实例化一个分享控件
+ * @param {domObject} element 用于创建分享控件的DOM元素
+ * @param {object}    options 分享空间配置参数
+ */
+ThinkShare = function(element, options){
+	//初始化配置参数
+	var self = this, ele = $(element), share_value;
+	var options = $.extend({}, defaults, options || {}, ele.data());
 
 	//加载CSS文件
-	var path = __FILE__.slice(0, __FILE__.lastIndexOf("/")) + "/css/share.css";
-	$("<link/>").attr({"href" : path, "type" : "text/css", "rel" : "stylesheet"}).appendTo("head");
+	includeCss("/css/share.css");
 
-	//执行默认分享工具
-	$($("meta[name='think-share']").attr("value") || "#think-share").ThinkShare();
+	//获取分享数据
+	share_value = _getShareData();
+
+	//解析字符串分享参数
+	if($.type(options.share) === "string") options.share = options.share.split(",");
+
+	//设置工具栏样式
+	ele.css("display", "inline-block").addClass("think-share-" + options.style);
+
+	//创建分享按钮
+	ele.append(_getShareBtnHTML());
+	
+	//获取分享数据
+	function _getShareData(){
+		var data     = {};
+		data.url     = encodeURIComponent(options.url || window.location.href);
+		data.title   = encodeURIComponent(options.title || $("title").text());
+		data.pic     = encodeURIComponent(options.pic || "");
+		data.summary = encodeURIComponent(options.summary || "");
+		_delete("url", "title", "pic", "summary");
+		return data;
+	}
+
+	//获取指定分享按钮html代码
+	function _getShareBtnHTML(){
+		var html = [options.header ? "<span>" + options.header + "</span>" : ""];
+		if(options.share.length == 0){
+			$.each(config, function(name){
+				html.push(_createShareBtn(name));
+			});
+		} else {
+			for(i in options.share){
+				html.push(_createShareBtn(options.share[i]));
+			}
+		}
+		return html.join("");
+	}
+
+	//创建分享按钮
+	function _createShareBtn(name){
+		return [
+			"<a class=\"share-icon-",
+			name,
+			"\" target=\"_blank\" href=\"",
+			_getShareUrl(name),
+			"\">",
+			config[name].title,
+			"</a>"
+		].join("");
+	}
+
+	//获取分享url
+	function _getShareUrl(name){
+		var param = params[name](share_value);
+		return [config[name].api, "?", param].join("");
+	}
+
+	//删除选项
+	function _delete(){
+		for(i in arguments){
+			delete options[arguments[i]];
+		}  
+	}
+}
+
+/* 定义JQUERY分享插件 */
+$.fn.ThinkShare = function(options){
+	$(this).each(function(){
+		new ThinkShare(this, options);
+	});
+}
+
+//加载指定的CSS文件
+function includeCss(css){
+	var path = __FILE__.slice(0, __FILE__.lastIndexOf("/"));
+	if($("link[href='" + path + css + "']").length) return;
+
+	//加载CSS文件
+	$("<link/>").attr({
+			"href" : path + css,
+			"type" : "text/css", 
+			"rel"  : "stylesheet"
+		}).appendTo("head");
+}
+
+//执行默认分享工具
+$($("meta[name='think-share']").attr("value") || "#think-share").ThinkShare();
 	
 })(jQuery);
